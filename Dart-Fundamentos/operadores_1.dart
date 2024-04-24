61b5b3b551439429f76fa3f6dc7cc5ac731a2de8
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-24 09:21:54 -0300

    Create operadores_1.dart

---
+++ Dart-Fundamentos/operadores_1.dart
@@ -0,0 +1,13 @@
+main(){
+  
+ // Operadores Lógicos
+ bool ehFragil = true;
+ bool ehCaro = true;
+  
+ print(ehFragil && ehCaro); // AND -> E
+ print(ehFragil || ehCaro); // OR -> OU
+ print(ehFragil ^ ehCaro); // XOR -> OU Exclusivo
+ print(!ehFragil); // NOT -> Unário/Prefix
+ print(!!ehCaro); 
+  
+}
