ca033d3661ce60629d3067352fb3aa2edc06cd72
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-24 11:53:43 -0300

    Create desafio_classe_carro.dart

---
+++ Dart-Orientação-A-Objetos/desafio_classe_carro.dart
@@ -0,0 +1,15 @@
+ /* 
+
+  1) carro.dart
+  Classe com construtor recebe a velocidade maxima...
+  final int velocidadeMaxima;
+  int velocidadeAtual;
+  
+  int acelerar() { ... } // +5
+  int frear() { ... } // -5
+  bool estaNoLimite()
+  
+  2) teste_carro.dart
+  main
+  
+*/
