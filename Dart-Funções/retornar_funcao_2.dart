e37d2e1b5e1a2d08f77d1ee7501286e6cdbeed99
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-24 10:46:56 -0300

    Create retornar_funcao_2.dart

---
+++ Dart-Funções/retornar_funcao_2.dart
@@ -0,0 +1,32 @@
+int Function(int) somaParcial(int a){
+  int c = 0;
+  
+  return (int b){
+    return a + b + c;
+  };
+}
+
+main(){
+  
+  print(somaParcial(2)(10));
+
+  // Resultado
+  // 12
+  
+  var somaCom10 = somaParcial(10);
+  
+  print(somaCom10(3));
+
+  // Resultado
+  // 13
+
+  print(somaCom10(7));
+
+  // Resultado
+  // 17
+
+  print(somaCom10(19));
+
+  // Resultado
+  // 29
+}
