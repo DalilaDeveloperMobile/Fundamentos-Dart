8f17b901aef88e434cd1f26f063b026c51ff2503
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-24 10:15:50 -0300

    Create algoritmo_e_Funcoes.dart

---
+++ Dart-Funções/algoritmo_e_Funcoes.dart
@@ -0,0 +1,4 @@
+// A mesma função pode ser chamada em varios lugares no código.
+// Chamando o nome da função, Ex: nome(). 
+// A função pode ter parametro de entrada e saida.
+// ( entrada = parametros ) e ( saida = return ). 
