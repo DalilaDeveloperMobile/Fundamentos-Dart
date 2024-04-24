da1a0a172053ed0c8267cf90251c432f9d2c6bdc
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-24 09:56:22 -0300

    Create for_4.dart

---
+++ Dart-Estrutura-De-Controle/for_4.dart
@@ -0,0 +1,31 @@
+main(){
+/*  
+ 
+  #
+  ##
+  ###
+  ####
+  #####
+  ######
+    
+  Use o laço FOR, mas não pode controlar o laço usando valor numérico!   
+  
+*/ 
+  
+  for(var a = '#'; a != '#######'; a+= '#'){
+    print(a);
+  }
+}
+
+/*  
+
+Resultado
+
+  #
+  ##
+  ###
+  ####
+  #####
+  ######
+
+*/ 
