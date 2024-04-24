d18afac9abc19408976a72e74016d9ce9aa74880
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-24 10:39:34 -0300

    Create funcao_em_variavel_2.dart

---
+++ Dart-Funções/funcao_em_variavel_2.dart
@@ -0,0 +1,27 @@
+main() {
+  
+  var adicao = (int a, int b) => a + b;
+  var subtracao = (int a, int b) => a - b;
+  var mutiplicacao = (int a, int b) => a * b;
+  var divisao = (int a, int b) => a / b;
+  
+  print(adicao(4, 19));
+
+  // Resultado
+  // 23
+
+  print(subtracao(9, 13));
+
+  // Resultado
+  // -4
+
+  print(mutiplicacao(9, 13));
+
+  // Resultado
+  // 117
+
+  print(divisao(9, 13));
+
+  // Resultado
+  // 0.6923076923076923
+}
